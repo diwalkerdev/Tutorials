65dbd68b98163bda36778e1115311f5ce04927dc
David Walker <david.walker@gmail.com>
2020-08-23 09:54:31 +0100

    Code at the end of episode 1.

--- multreturns.cpp
+++ multreturns.cpp
@@ -1,8 +1,11 @@
 #include <array>
+#include <span>
 #include <stdio.h>
 
 // Multiple return types using if constexpr
 // ----------------------------------------
+// Code:
+// https://github.com/diwalkerdev/Tutorials
 // Notes:
 // 1 - floating point / arithmetic concept
 // 2 - operator [] using span.
@@ -10,7 +13,7 @@
 
 //////////////////////////////////////////////////////////////////////////////
 
-template <typename Tp, std::size_t M, std::size_t N>
+template <std::floating_point Tp, std::size_t M, std::size_t N>
 struct Matrix {
     using value_type = Tp;
     using reference  = value_type&;
@@ -19,9 +22,20 @@ struct Matrix {
     // TODO: redundancy, both matrix and array store size parameter.
     std::array<Tp, M * N> _elems;
 
-    auto operator[](size_type pos) -> reference
+    auto operator[](size_type pos)
     {
-        return _elems[pos];
+        if constexpr (N == 1)
+        {
+            return _elems[pos];
+        }
+        else if constexpr (M == 1)
+        {
+            return _elems[pos];
+        }
+        else
+        {
+            return std::span<float, N> {&_elems[pos * N], N};
+        }
     }
 };
 
@@ -34,7 +48,12 @@ int main()
     // Lazy, will improve on this later.
     A._elems = {1, 2, 3, 4};
 
-    printf("%f\n", A[0]);
+    printf("%f\n", A[0][1]);
+
+    Matrix<float, 1, 4> B;
+    B._elems = {5, 6, 7, 8};
+
+    printf("%f\n", B[0]);
 
     return 0;
 }
